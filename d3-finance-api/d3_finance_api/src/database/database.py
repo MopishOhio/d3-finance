ae146aa380c2f2ee949481343dcb606151311ae6
Francisco Lucas Janesch Lange Sens <francisco.lucas.sens@gmail.com>
2025-05-13 19:42:50 -0300

    Adicionado postgresql

--- d3-finance-api/d3_finance_api/src/database/database.py
+++ d3-finance-api/d3_finance_api/src/database/database.py
@@ -12,11 +12,11 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")  # O valor padrão para a porta do MySQL é 3306
+DB_PORT = os.getenv("DB_PORT", "5432")  # O valor padrão para a porta do MySQL é 3306
 DB_NAME = os.getenv("DB_NAME")
 
 # Update with your MySQL credentials and database name
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
